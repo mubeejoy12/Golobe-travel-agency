fe32ca7c36799bab1afa281739c6bd61c3b05b79
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2023-12-12 22:57:42 +0100

    adding up to the design

--- src/Pages/FirstPage.jsx
+++ src/Pages/FirstPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Stack, Box } from "@mui/material";
+import { Stack, Box, Typography } from "@mui/material";
 import globeImage from "../assets/Rectangle.png";
+import airplane from "../assets/airplane.png";
 
 const containerStyle = {
   width: "1380px",
@@ -11,13 +12,43 @@ const containerStyle = {
   backgroundImageRepeat: "no-repeat",
   backgroundPosition: "center",
   backgroundSize: "cover",
-  borderRadius: "24px"
-  
+  borderRadius: "24px",
+  marginTop: "30px",
+};
 
+const navBar = {
+  border: "2px solid blue",
+  marginTop: "30px",
+  height: "96px",
+  display: "flex",
+  flexDirection: "row",
+};
+const itemOne = {
+  border: "2px solid yellow",
+  width: "242px",
+};
+const itemTwo = {
+  border: "2px solid purple",
+};
+const itemThree = {
+  border: "2px solid red",
 };
 
 const FirstPage = () => {
-  return <Stack sx={containerStyle}></Stack>;
+  return (
+    <Stack sx={containerStyle}>
+      <Stack sx={navBar}>
+        <Stack sx={itemOne}>
+          <img src={airplane} sx={{ width: "24px", height: "24px" }} alt="" />
+          <Typography variant="h9" color="initial">
+            findFlight
+          </Typography>
+        </Stack>
+        <Stack sx={itemTwo}>hcxghvcg</Stack>
+        <Stack sx={itemThree}>chbcgbvdsyb</Stack>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default FirstPage;
