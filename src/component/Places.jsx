7cdab96870700c971878aa5cf79e0cbb9a5d02dd
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-09 16:52:45 +0100

    chore: imported the images to places

--- src/component/Places.jsx
+++ src/component/Places.jsx
@@ -1,6 +1,15 @@
 import { Box, Grid, Paper } from "@mui/material";
 import React from "react";
 import Cards from "./Cards";
+import instabulimg1 from "../assets/instabulimg.png";
+import instabulimg2 from "../assets/Sybney.png";
+import instabulimg3 from "../assets/Baku.png";
+import instabulimg4 from "../assets/male.png";
+import instabulimg5 from "../assets/Paris.png";
+import instabulimg6 from "../assets/Newyork.png";
+import instabulimg7 from "../assets/London.png";
+import instabulimg8 from "../assets/Tokyo.png";
+import instabulimg9 from "../assets/Dubai.png";
 
 const Places = () => {
   return (
