60b893b68dc8a7aed67559cf6dbebf81e35a723b
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-09 12:05:33 +0100

    chore: using icon as aprops

--- src/component/functions/CustomInput.jsx
+++ src/component/functions/CustomInput.jsx
@@ -3,3 +3,14 @@ import BedIcon from "@mui/icons-material/Bed";
 
 
 const CustomInput = () => {
+  return (
+    <div>
+      <BedIcon {...props} />
+      <Icon iconName="swap"
+        style={{ position: "absolute", right: 10, top: 14, color: "#757575" }}
+      />
+    </div>
+  );
+};
+
+export default CustomInput;
