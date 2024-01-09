6145eb75142bf75dfeaf4614b14d9cc206a4c843
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-09 11:53:38 +0100

    chore: added grid system to places

---
+++ src/component/Places.jsx
@@ -0,0 +1,35 @@
+import { Box, Grid } from "@mui/material";
+import React from "react";
+
+const Places = () => {
+  return (
+    <Box sx={{ border: "2px solid green" }}>
+      <Grid container spacing={2}>
+        {" "}
+        {/* Adjust spacing as needed */}
+        <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+          ghjfvhvhj
+          {/* Content for the first grid item */}
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+          samf as
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+          nsajn sd
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+          ghjfvhvhj
+          {/* Content for the first grid item */}
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+          samf as
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
+          nsajn sd
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Places;
