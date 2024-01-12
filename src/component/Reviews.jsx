791e8c7ac359ed7342002eb484ea80f0f7250b00
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-12 13:30:37 +0100

    chore:imported images to review component

--- src/component/Reviews.jsx
+++ src/component/Reviews.jsx
@@ -2,8 +2,54 @@ import { Grid, Stack } from "@mui/material";
 import React from "react";
 import HeadingTitle from "./HeadingTitle";
 import ReviewCrad from "./ReviewCrad";
+import cloudOne from "../assets/cloudOne.png";
+import cloudTwo from "../assets/cloudTwo.png";
+import cloudThree from "../assets/cloudThree.png";
 
 const Reviews = () => {
+  const reviewsData = [
+    {
+      reviewImage: cloudOne,
+      heading: "“A real sense of community, nurtured”",
+      subtitle:
+        "Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for ..",
+      name: "Olga",
+      location: "Weave Studios – Kai Tak",
+    },
+    {
+      reviewImage: cloudTwo,
+      heading: "“The facilities are superb. Clean, slick, bright.”",
+      subtitle:
+        "“A real sense of community, nurtured”Really appreciate the help and support from the staff ..",
+      name: "Thomas",
+      location: "Weave Studios – Olympic",
+    },
+    {
+      reviewImage: cloudThree,
+      heading: "“A real sense of community, nurtured”",
+      subtitle:
+        "Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for ..",
+      name: "Eliot",
+      location: "Weave Studios – Kai Tak",
+    },
+    {
+      reviewImage: cloudThree,
+      heading: "“A real sense of community, nurtured”",
+      subtitle:
+        "Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for ..",
+      name: "Eliot",
+      location: "Weave Studios – Kai Tak",
+    },
+
+    {
+      reviewImage: cloudThree,
+      heading: "“A real sense of community, nurtured”",
+      subtitle:
+        "Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for ..",
+      name: "Eliot",
+      location: "Weave Studios – Kai Tak",
+    },
+  ];
   return (
     <Stack
       sx={{ width: "1396px", height: "722px", border: "2px solid yellow" }}
