a6b8dd5e932b9d26dded7dd4b5f4546e4d873125
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-12 13:22:45 +0100

    added  footer component render subfooter in it

---
+++ src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Subfooter from './Subfooter'
+import { Stack } from '@mui/material'
+
+const Footer = () => {
+  return (
+    <Stack>
+ <Subfooter/>
+    </Stack>
+  )
+}
+
+export default Footer
