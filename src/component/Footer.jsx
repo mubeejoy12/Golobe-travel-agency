d5962990bd2f56082af967d52415273b4c7e21e2
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-22 17:44:32 +0100

    render the main footer conponents on the footer component

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Subfooter from './Subfooter'
 import { Stack } from '@mui/material'
+import MainFooter from './MainFooter'
 
 const Footer = () => {
   return (
     <Stack>
  <Subfooter/>
+ <MainFooter/>
     </Stack>
   )
 }
