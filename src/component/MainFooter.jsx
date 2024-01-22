3fafc9064170d8595bc02925ea0ed0f4d4cc2c87
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-01-22 17:47:42 +0100

    imported the images to mainfooter component

---
+++ src/component/MainFooter.jsx
@@ -0,0 +1,5 @@
+import blackLogo from "../assets/BackGolobe.png";
+import facebook from "../assets/facebook.png";
+import twitter from "../assets/x.png";
+import instagram from "../assets/instagram.png";
+import youtube from "../assets/youtube.png";
